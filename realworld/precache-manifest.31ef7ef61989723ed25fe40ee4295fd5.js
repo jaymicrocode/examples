e4f4564f7230261dc90928011e9ebf28dede0795
Travis CI User <travis@example.org>
2020-01-21 15:43:27 +0000

    built example

---
+++ realworld/precache-manifest.31ef7ef61989723ed25fe40ee4295fd5.js
@@ -0,0 +1,64 @@
+self.__precacheManifest = [
+  {
+    "url": "src/widgets/Article.5dd1b7cc2f71412409d5.bundle.js"
+  },
+  {
+    "url": "icon_128x128.3789c4a4f67429a0cd993c52d2c0d2a0.png"
+  },
+  {
+    "url": "main.098bc37731419d7cc0a2.bundle.js"
+  },
+  {
+    "url": "runtime/IntersectionObserver.5a73e422c4a2e88ab3aa.bundle.js"
+  },
+  {
+    "url": "runtime/ResizeObserver.ad5a9478edabdb73b308.bundle.js"
+  },
+  {
+    "url": "runtime/WebAnimations.9758bc16b2d970662502.bundle.js"
+  },
+  {
+    "url": "runtime/blocks.2ba08f1c56d543c794fe.bundle.js"
+  },
+  {
+    "url": "runtime/client.0f21fbae279c18c43d10.bundle.js"
+  },
+  {
+    "url": "runtime/fetch.0750045559eee54f142f.bundle.js"
+  },
+  {
+    "url": "runtime/pointerEvents.6b8c798827bb2884fabf.bundle.js"
+  },
+  {
+    "url": "icon_256x256.c3399fc140e7b7ce69760eb55c9e94cd.png"
+  },
+  {
+    "url": "src/widgets/Home.0f78d153e6d227b46200.bundle.js"
+  },
+  {
+    "url": "src/widgets/Login.b55ebd16b9099284995b.bundle.js"
+  },
+  {
+    "url": "src/widgets/Profile.8d3c7fdaa136e37fd248.bundle.js"
+  },
+  {
+    "url": "src/widgets/Register.0369b14febb60cb858c4.bundle.js"
+  },
+  {
+    "url": "src/widgets/Settings.6307a817628ec0f78a12.bundle.js"
+  },
+  {
+    "revision": "3b7a9c7725a3e5176c91f496a1957457",
+    "url": "index.html"
+  },
+  {
+    "url": "icon_512x512.4b938ed405cd7a04e8c706a0097e2bc5.png"
+  },
+  {
+    "url": "bootstrap.bb4d753f1a245e4b954f.bundle.js"
+  },
+  {
+    "revision": "bb4d753f1a245e4b954f",
+    "url": "bootstrap.6a8d73655f83e7b55159.bundle.css"
+  }
+];
